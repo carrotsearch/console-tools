6f1d31c80d47dcaba951ed680f380917ca7fa212
Dawid Weiss <dawid.weiss@carrotsearch.com>
2019-07-25 15:04:01 +0200

    Add tabular output as a subproject.:

---
+++ formatters/src/main/java/com/carrotsearch/console/formatters/TabularOutput.java
@@ -0,0 +1,346 @@
+/*
+ * console-tools
+ *
+ * Copyright (C) 2019, Carrot Search s.c.
+ * All rights reserved.
+ */
+package com.carrotsearch.console.formatters;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+/** Tabular output data dump with automatically adjusted column widths and some other utilities. */
+public final class TabularOutput {
+  /** Column separator. */
+  private final String columnSeparator;
+
+  /** Values for the current row. */
+  private final List<Object> currentRow = new ArrayList<>();
+
+  /** Buffered rows. */
+  private final List<List<Object>> data = new ArrayList<>();
+
+  /** A writer to write the output to. */
+  private final Writer writer;
+
+  /** Flush rows every n-th line. */
+  private final int flushCount;
+
+  /** */
+  private final boolean outputHeader;
+
+  private final List<ColumnData> columns;
+
+  private static final class ColumnData {
+    final String name;
+    final ColumnSpec spec;
+    int width;
+
+    public ColumnData(String name, ColumnSpec spec) {
+      this.name = name;
+      this.spec = spec;
+    }
+  }
+
+  /** Column alignment. */
+  public static enum Alignment {
+    LEFT,
+    RIGHT,
+    CENTER;
+  }
+
+  /** Column specification. */
+  public static final class ColumnSpec {
+    /** Alignment. */
+    Alignment alignment = Alignment.LEFT;
+
+    /** Formatter for the value. */
+    String format = "%s";
+
+    /** Sets column flush on the last added column. */
+    public ColumnSpec alignLeft() {
+      this.alignment = Alignment.LEFT;
+      return this;
+    }
+
+    /** Sets column flush on the last added column. */
+    public ColumnSpec alignRight() {
+      this.alignment = Alignment.RIGHT;
+      return this;
+    }
+
+    /** Sets column flush on the last added column. */
+    public ColumnSpec alignCenter() {
+      this.alignment = Alignment.CENTER;
+      return this;
+    }
+
+    public ColumnSpec format(String formatString) {
+      format = Objects.requireNonNull(formatString);
+      return this;
+    }
+  }
+
+  public static class Builder {
+    private Writer writer;
+    private boolean outputHeader = true;
+    private int flushCount = 1;
+    private String columnSeparator = " ";
+    private LinkedHashMap<String, ColumnSpec> columnsByName = new LinkedHashMap<>();
+
+    public Builder(Writer writer) {
+      this.writer = Objects.requireNonNull(writer);
+    }
+
+    /** Emit or skip the header. */
+    public Builder outputHeaders(boolean outputHeader) {
+      this.outputHeader = outputHeader;
+      return this;
+    }
+
+    /**
+     * Flush automatically every n-lines.
+     *
+     * @see #flush()
+     */
+    public Builder flushEvery(int n) {
+      this.flushCount = n;
+      return this;
+    }
+
+    /** Don't flush lines automatically. */
+    public Builder noAutoFlush() {
+      return flushEvery(Integer.MAX_VALUE);
+    }
+
+    public Builder columnSeparator(String separator) {
+      this.columnSeparator = separator;
+      return this;
+    }
+
+    /** Adds a column to the tabular's layout. */
+    public Builder addColumn(String name, Consumer<ColumnSpec> columnConfig) {
+      if (columnsByName.containsKey(name)) {
+        throw new IllegalArgumentException("Two columns with the same name: " + name);
+      }
+
+      ColumnSpec cs = new ColumnSpec();
+      columnConfig.accept(cs);
+      columnsByName.put(name, cs);
+      return this;
+    }
+
+    public Builder addColumn(String name) {
+      return addColumn(name, t -> {});
+    }
+
+    public TabularOutput build() {
+      return new TabularOutput(writer, columnsByName, columnSeparator, flushCount, outputHeader);
+    }
+
+    public Builder addColumns(String... names) {
+      Arrays.stream(names).forEach(this::addColumn);
+      return this;
+    }
+
+    public Builder addColumns(List<String> names) {
+      names.forEach(this::addColumn);
+      return this;
+    }
+  }
+
+  public static Builder to(Writer writer) {
+    return new Builder(writer);
+  }
+
+  /** Where to write the output to. */
+  private TabularOutput(
+      Writer writer,
+      LinkedHashMap<String, ColumnSpec> columns,
+      String colSeparator,
+      int flushCount,
+      boolean outputHeader) {
+    this.writer = writer;
+    this.outputHeader = outputHeader;
+    this.flushCount = flushCount;
+    this.columnSeparator = colSeparator;
+    this.columns =
+        columns.entrySet().stream()
+            .map((e) -> new ColumnData(e.getKey(), e.getValue()))
+            .collect(Collectors.toList());
+  }
+
+  public TabularOutput append(Object... values) {
+    this.currentRow.addAll(Arrays.asList(values));
+    if (currentRow.size() > columns.size()) {
+      throw new RuntimeException(
+          "Current row has more values than declared columns: " + currentRow);
+    }
+    return this;
+  }
+
+  public TabularOutput nextRow() {
+    while (currentRow.size() < columns.size()) {
+      currentRow.add(null);
+    }
+    data.add(new ArrayList<>(currentRow));
+    currentRow.clear();
+    if (data.size() >= flushCount) {
+      flush();
+    }
+    return this;
+  }
+
+  public Writer getWriter() {
+    return writer;
+  }
+
+  public TabularOutput flush() {
+    try {
+      flush0();
+      getWriter().flush();
+      return this;
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+  }
+
+  private void flush0() throws IOException {
+    if (!currentRow.isEmpty()) {
+      throw new RuntimeException("Unflushed data in the current row. Very likely a bug.");
+    }
+
+    if (data.isEmpty()) {
+      return;
+    }
+
+    // Check if column widths have changed.
+    List<List<String>> formatted = new ArrayList<>();
+    boolean columnWidthsChanged = false;
+    for (List<Object> row : data) {
+      List<String> formattedRow = new ArrayList<>();
+      formatted.add(formattedRow);
+
+      assert row.size() == columns.size();
+      for (int i = 0; i < row.size(); i++) {
+        ColumnData colData = columns.get(i);
+        String value = formatValue(colData, row.get(i));
+        formattedRow.add(value);
+
+        int vlen = value.length();
+        if (vlen > colData.width) {
+          colData.width = vlen;
+          columnWidthsChanged = true;
+        }
+      }
+    }
+    data.clear();
+
+    if (columnWidthsChanged && outputHeader) {
+      for (ColumnData cd : columns) {
+        cd.width = Math.max(cd.name.length(), cd.width);
+      }
+
+      writer.write(
+          String.join(
+              columnSeparator,
+              columns.stream()
+                  .map((d) -> align(d.spec.alignment, d.name, d.width))
+                  .collect(Collectors.toList())));
+      writer.write("\n");
+    }
+
+    for (List<String> row : formatted) {
+      for (int i = 0; i < columns.size(); i++) {
+        ColumnData cd = columns.get(i);
+        row.set(i, align(cd.spec.alignment, row.get(i), cd.width));
+      }
+
+      writer.write(String.join(columnSeparator, row));
+      writer.write("\n");
+    }
+  }
+
+  private String align(Alignment alignment, String string, int width) {
+    switch (alignment) {
+      case LEFT:
+        return padEnd(string, width, ' ');
+      case RIGHT:
+        return padStart(string, width, ' ');
+      default:
+        return center(string, width);
+    }
+  }
+
+  private String formatValue(ColumnData columnData, Object v) {
+    v = toStringAdapter(v);
+
+    String value;
+    if (v == null) {
+      value = "--";
+    } else {
+      value = String.format(Locale.ROOT, columnData.spec.format, v);
+    }
+
+    return value;
+  }
+
+  private Object toStringAdapter(Object object) {
+    if (object == null) return object;
+    if (object instanceof char[]) return new String((char[]) object);
+    if (object instanceof byte[]) return Arrays.toString((byte[]) object);
+    if (object instanceof short[]) return Arrays.toString((short[]) object);
+    if (object instanceof int[]) return Arrays.toString((int[]) object);
+    if (object instanceof long[]) return Arrays.toString((long[]) object);
+    if (object instanceof float[]) return Arrays.toString((float[]) object);
+    if (object instanceof double[]) return Arrays.toString((double[]) object);
+    return object;
+  }
+
+  private static String padStart(String string, int minLength, char padChar) {
+    Objects.requireNonNull(string);
+    if (string.length() >= minLength) {
+      return string;
+    }
+    StringBuilder sb = new StringBuilder(minLength);
+    for (int i = string.length(); i < minLength; i++) {
+      sb.append(padChar);
+    }
+    sb.append(string);
+    return sb.toString();
+  }
+
+  private static String padEnd(String string, int minLength, char padChar) {
+    Objects.requireNonNull(string); // eager for GWT.
+    if (string.length() >= minLength) {
+      return string;
+    }
+    StringBuilder sb = new StringBuilder(minLength);
+    sb.append(string);
+    for (int i = string.length(); i < minLength; i++) {
+      sb.append(padChar);
+    }
+    return sb.toString();
+  }
+
+  private String center(String s, int size) {
+    StringBuilder sb = new StringBuilder(size);
+    for (int i = (size - s.length()) / 2; i > 0; i--) {
+      sb.append(' ');
+    }
+    sb.append(s);
+    while (sb.length() < size) {
+      sb.append(' ');
+    }
+    return sb.toString();
+  }
+}
---
+++ formatters/src/test/java/com/carrotsearch/console/formatters/TabularOutputTest.java
@@ -0,0 +1,145 @@
+/*
+ * console-tools
+ *
+ * Copyright (C) 2019, Carrot Search s.c.
+ * All rights reserved.
+ */
+package com.carrotsearch.console.formatters;
+
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class TabularOutputTest {
+  @Test
+  public void oneRow() {
+    checkEquals(
+        (sw) -> TabularOutput.to(sw).outputHeaders(false).addColumns("A", "B").build(),
+        (to) -> {
+          to.append("a", "b").nextRow();
+        },
+        "a b");
+  }
+
+  @Test
+  public void changedWidth() {
+    checkEquals(
+        (sw) ->
+            TabularOutput.to(sw)
+                .outputHeaders(false)
+                .columnSeparator("|")
+                .addColumns("A", "B")
+                .build(),
+        (to) -> {
+          to.append("a", "bb").nextRow();
+          to.append("aa", "b").nextRow();
+        },
+        "a|bb",
+        "aa|b ");
+  }
+
+  @Test
+  public void headers() {
+    checkEquals(
+        (sw) -> TabularOutput.to(sw).columnSeparator("|").addColumns("A", "B").build(),
+        (to) -> {
+          to.append("a", "b").nextRow();
+        },
+        "A|B",
+        "a|b");
+  }
+
+  @Test
+  public void headersUpdateAfterSizedChanged() {
+    checkEquals(
+        (sw) -> TabularOutput.to(sw).columnSeparator("|").addColumns("A", "B").build(),
+        (to) -> {
+          to.append("a", "b").nextRow();
+          to.append("aa", "bb").nextRow();
+        },
+        "A|B",
+        "a|b",
+        "A |B ",
+        "aa|bb");
+  }
+
+  @Test
+  public void noAutoflushAlignments() {
+    checkEquals(
+        (sw) ->
+            TabularOutput.to(sw)
+                .columnSeparator("|")
+                .noAutoFlush()
+                .addColumn("A", c -> c.alignLeft())
+                .addColumn("B", c -> c.alignCenter())
+                .addColumn("C", c -> c.alignRight())
+                .build(),
+        (to) -> {
+          to.append("a", "b", "c").nextRow();
+          to.append("aa", "bb", "cc").nextRow();
+          to.append("aaa", "bbb", "ccc").nextRow();
+          to.flush();
+        },
+        "A  | B |  C",
+        "a  | b |  c",
+        "aa |bb | cc",
+        "aaa|bbb|ccc");
+  }
+
+  @Test
+  public void customFormatter() {
+    checkEquals(
+        (sw) ->
+            TabularOutput.to(sw)
+                .columnSeparator("|")
+                .noAutoFlush()
+                .outputHeaders(false)
+                .addColumn("A", c -> c.format("%6.2f"))
+                .build(),
+        (to) -> {
+          to.append(1.2f).nextRow();
+          to.flush();
+        },
+        "  1.20");
+  }
+
+  @Test
+  public void headersLongerThanData() {
+    checkEquals(
+        (sw) ->
+            TabularOutput.to(sw)
+                .columnSeparator("|")
+                .outputHeaders(true)
+                .addColumn("AAA", c -> c.alignCenter())
+                .build(),
+        (to) -> {
+          to.append("a").nextRow();
+          to.flush();
+        },
+        "AAA",
+        " a ");
+  }
+
+  private void checkEquals(
+      Function<Writer, TabularOutput> configure,
+      Consumer<TabularOutput> appender,
+      String... expectedRows) {
+    StringWriter sw = new StringWriter();
+    TabularOutput to = configure.apply(sw);
+    appender.accept(to);
+
+    String expected = Arrays.stream(expectedRows).map(e -> e + "\n").collect(Collectors.joining());
+    String actual = sw.toString();
+    if (!Objects.equals(expected, actual)) {
+      System.out.println("Expected:\n" + expected);
+      System.out.println("Actual  :\n" + actual);
+      Assertions.assertThat(actual).isEqualTo(expected);
+    }
+  }
+}
